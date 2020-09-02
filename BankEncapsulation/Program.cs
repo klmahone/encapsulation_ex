ac31e569ea6f42bb7f46023d33c63cd5a37b4b0d
klmahone <klmahone@gmail.com>
2020-09-02 15:07:15 -0400

    second commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -6,7 +6,17 @@ namespace BankEncapsulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var kevinsAccount = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            kevinsAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is now {kevinsAccount.GetBalance()}");
+
+
         }
     }
 }
